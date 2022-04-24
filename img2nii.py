135f763850f3090b658af6780af466528ec62d3e
Erjun Zhang <zhangerjun2014@gmail.com>
2022-04-24 18:23:05 -0400

    correc resolution of nii and normalization float32

--- img2nii.py
+++ img2nii.py
@@ -10,8 +10,11 @@ def img2nii(folder,image_prefix,save_nii,save_name,save_path):
     '''
     images = []
     for filename in np.sort(os.listdir(folder)):
-        img = cv2.imread(os.path.join(folder,filename),cv2.IMREAD_GRAYSCALE)
-        if (img is not None) and (image_prefix in filename):
+        if (image_prefix in filename):
+            if filename[-4:] == '.hdr':
+                img = cv2.imread(os.path.join(folder,filename),cv2.IMREAD_ANYDEPTH)[:,:,0]
+            else:
+                img = cv2.imread(os.path.join(folder,filename),cv2.IMREAD_GRAYSCALE)
             images.append(img)
     images_xyz = np.array(images).swapaxes(0,1).swapaxes(1,2)
     #-----------------
@@ -25,8 +28,10 @@ def img2nii(folder,image_prefix,save_nii,save_name,save_path):
     #-----------------
     img = nib.Nifti1Image(images_xyz, np.eye(4))
     if save_nii == True:
+        img.header['xyzt_units'] = 11
         img.header['dim'][0] = 3#[3,128,128,14,1,1,0,0]
         img.header['dim'][4] = 1#[3,128,128,14,1,1,0,0]
+        img.header['pixdim'] = [1,0.004,0.004,0.050,1,1,1,1]
         save_nifti(os.path.join(save_path,save_name+'.nii'), images_xyz.astype(np.float32), 
                    affine=np.eye(4),hdr=img.header)
     return images_xyz
